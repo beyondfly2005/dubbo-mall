99633b643f389472b5f099f306f5e6e781e1e8f8
beyondSoft <498601485@qq.com>
2020-08-24 21:34:52 +0800

    RabbitMQ Topic

---
+++ dubbo-mall-temp/rabbitmq-java/src/main/java/com/beyonsoft/rabbitmq/topic/Recver1.java
@@ -0,0 +1,45 @@
+package com.beyonsoft.rabbitmq.topic;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Recver1 {
+
+    //简单交换机
+    private final static String EXCHANGE_NAME="topic_exchange";
+    //简单队列
+    private final static String QUEUE_NAME = "topic_exchange_queue_1";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //1、连接MQ服务器
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setVirtualHost("/admin");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setHost("192.168.0.108");
+        factory.setPort(5672);
+        //2、发送消息给MQ服务器
+        Connection connection = factory.newConnection();
+        //3、基于channel 类似会话的作用
+        Channel channel = connection.createChannel();
+        //声明队列 并且将队列绑定到交换机上
+        channel.queueDeclare(QUEUE_NAME,true,false,false,null);
+        channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,"nba.*");
+        //channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,"nba.#");
+        //4、声明消费者
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                //处理接收到的消息
+                String msg = new String(body,"utf-8");
+                System.out.println("Recv1==> 接收到的消息为："+msg);
+            }
+        };
+        //5、让消费者监听队列
+        //autoAck 自动回复 true 另外还有手工模式
+        channel.basicConsume(QUEUE_NAME,true,consumer);
+
+    }
+}
---
+++ dubbo-mall-temp/rabbitmq-java/src/main/java/com/beyonsoft/rabbitmq/topic/Sender.java
@@ -0,0 +1,39 @@
+package com.beyonsoft.rabbitmq.topic;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 默认轮流发送给消费者
+ * 我们希望处理快的多处理一些，处理慢的少处理一些
+ */
+public class Sender {
+
+    //简单交换机
+    private final static String EXCHANGE_NAME="topic_exchange";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //1、连接MQ服务器
+        ConnectionFactory factory =new ConnectionFactory();
+        factory.setVirtualHost("/admin");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setHost("192.168.0.108");
+        factory.setPort(5672);
+
+        //2、发送消息给MQ服务器
+        Connection connection=factory.newConnection();
+        //3、基于channel 类似会话的作用
+        Channel channel = connection.createChannel();
+        //4、声明交换机
+        channel.exchangeDeclare(EXCHANGE_NAME,"topic");
+        //5、发布消息
+        channel.basicPublish(EXCHANGE_NAME,"nba.add",null,"克莱宣布加盟湖人队！".getBytes());
+        channel.basicPublish(EXCHANGE_NAME,"cba.add",null,"周琦宣布加盟湖公洞宏远！".getBytes());
+        System.out.println("消息发送完毕");
+    }
+}
